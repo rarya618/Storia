9f6a23f98b3d2983a0577cf86e44555fef7f7c67
Russal Arya <russalarya@gmail.com>
2022-02-16 08:51:52 +0300

    Converted Button to tsx

---
+++ src/objects/Button.tsx
@@ -0,0 +1,23 @@
+import React, { MouseEventHandler } from "react";
+
+type Props = {
+    onClick: MouseEventHandler,
+    text: string,
+    color: string,
+    border: string,
+    id: string
+}
+
+const Button = ({color, onClick, text, border, id}: Props) => {
+    <button 
+        className={"button relative-button medium " 
+        + color + "-color " 
+        + color + "-view " 
+        + border + "-border round-5px small-spaced no-select"} 
+        onClick={onClick} 
+        id={id}>
+        {text}
+    </button>
+}
+
+export default Button;
